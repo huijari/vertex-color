6e99416a45108e1e62184461e912eb809b0519a9
Alexandre Cesar <acmorcego@gmail.com>
2018-07-02 01:57:38 -0300

    assembly main

--- src/main.lisp
+++ src/main.lisp
@@ -1,1 +1,18 @@
-(function main () -> int (return 0))
+(include "io.h")
+(include "list.h")
+(include "node.h")
+(include "heuristic.h")
+
+(include <stdio.h>)
+(include <stdlib.h>)
+
+(function main () -> int
+	(decl ((uint32_t vertices = (read_int))
+				 (uint32_t edges = (read_int))
+				 (Node** graph = (read_graph vertices edges))
+				 (uint32_t color = (welsh_powell graph vertices))))
+
+	(write_round color)
+	(write_allocation graph vertices)
+
+	(return 0))
